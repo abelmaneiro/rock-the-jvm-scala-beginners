fbf7679d9c8df2c156d524ba95f7dc4f0ca30abd
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-04-04 17:20:29 +0100

    3.16 Inheritance cont, Abstruct class and trait

---
+++ src/lectures/part2oop/s16AbstractDataTypes.scala
@@ -0,0 +1,33 @@
+package lectures.part2oop
+
+object s16AbstractDataTypes extends App{
+
+  abstract class Animal {  // abstract class contain undefined fields methods
+    val createType: String = "wild"
+    def eat(): Unit
+  }
+
+  class Dog extends Animal { // makes concrete abstract class
+    override val createType: String = "dog"
+    override def eat(): Unit = println("crunch woof")
+  }
+
+  trait Carnivore {  // trait is abstract
+    def eat(animal: Animal): Unit
+    val preferredMeal: String = "fresh meat"
+  }
+  trait ColdBlooded
+
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val createType: String = "crocodile"
+    override def eat(): Unit = println("crunch splash")
+    override def eat(animal: Animal): Unit = println(s"I am $createType and eating ${animal.createType}")
+  }
+
+  val dog = new Dog
+  val crocodile = new Crocodile
+  crocodile.eat()
+  crocodile.eat(dog)
+
+
+}
